eb3293d60f0fc1ddb98d1b6a3554c4999689d08d
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-16 18:33:17 -0600

    added helpers and partials

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const routes = require("./controllers/");
 const sequelize = require("./config/connection.js");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const helpers = require("./utils/helpers.js");
+const hbs = exphbs.create({ helpers });
 const session = require("express-session");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -17,8 +18,6 @@ const sess = {
     db: sequelize,
   }),
 };
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
